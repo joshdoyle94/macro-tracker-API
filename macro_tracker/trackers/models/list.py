66efe4b27f77aa67bd3f9dfe2b08072ed3f59044
akestler <andrew.kestler@generalassemb.ly>
2022-12-07 10:59:54 -0500

    see pull request for summary of CR

--- macro_tracker/trackers/models/list.py
+++ macro_tracker/trackers/models/list.py
@@ -10,8 +10,8 @@ class List(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        null=True,
-        blank=True
+        null=True, # do we want to have Lists unassociated with users ? probably not - how would that be useful ? ( are we seeding the db ?)
+        blank=True # ^
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
